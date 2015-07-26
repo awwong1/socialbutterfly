b9f44265d79ff627760baf31c3c7d5aa546b5e3a
Alexander Wong <admin@alexander-wong.com>
2015-07-26 01:22:02 -0600

    Readme modications, removed insecure, pushed to public

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 Social Butterfly
 ================
-A prototype social network coded for fun. Really a learning exercise for Meteor.js.
+A prototype chat application coded for fun. Really a learning exercise for Meteor.js.
 
 Currently hosted on [http://socialbutterfly.meteor.com/](http://socialbutterfly.meteor.com/).
+
+Chat window will roll over after 50 messages just in case you type something in that's embarrassing.
+No email signup necessary, just plop in a username and password and you're ready to go!
+
+License
+-------
+This project is distributed under [The MIT License](https://github.com/awwong1/socialbutterfly/blob/master/LICENSE).
--- templates/app/app.js
+++ templates/app/app.js
@@ -1,8 +1,34 @@
 Messages = new Mongo.Collection("messages");
 
+Meteor.methods({
+  addMessage: function (text) {
+    // Make sure the user is logged in before inserting a task
+    if (!Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Messages.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  }
+});
+
 if (Meteor.isClient) {
   Template.App.rendered = function () {
-
+    $('.new-message').form({
+      fields: {
+        text: {
+          identifier: 'text',
+          rules: [
+            {type: 'empty', prompt: 'Please enter some text'},
+            {type: 'maxLength[255]', prompt: 'Your text must be less than 255 characters'}
+          ]
+        }
+      }
+    });
     // fix main menu to page on passing
     $('.main.menu').visibility({
       type: 'fixed'
@@ -23,16 +49,11 @@ if (Meteor.isClient) {
     "submit .new-message": function (event) {
       // Prevent default browser form submit
       event.preventDefault();
-
       // Get value from form element
       var text = event.target.text.value;
 
       // Insert a task into the collection
-      Messages.insert({
-        username: Meteor.user().username,
-        text: text,
-        createdAt: new Date() // current time
-      });
+      Meteor.call("addMessage", text);
 
       // Clear form
       event.target.text.value = "";
